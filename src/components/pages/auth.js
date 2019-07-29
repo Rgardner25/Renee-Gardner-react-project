59af71c1e11d3d6eb2ca2a511a533486b67a31a8
Renee Gardner <rmg2588@gmail.com>
2019-07-29 15:20:09 -0500

    update project

---
+++ src/components/pages/auth.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+import Login from "../auth/login";
+import loginImg from "../../../static/assets/images/auth/login.jpg";
+
+export default class Auth extends Component {
+  render() {
+    return (
+        <div className="auth-page-wrapper">
+          <div
+            className="left-column"
+            style={{
+              backgroundImage: `url(${loginImg})`
+            }}
+          />
+  
+          <div className="right-column">
+            <Login>
+                
+            </Login>
+          </div>
+        </div>
+      );
+    }
+  }
---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import PortfolioItem from "./portfolio-item";
+
+export default class PortfolioContainer extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: false,
+      data: []
+    };
+
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    });
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("https://reneegardner.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
+        this.setState({
+          data: response.data.portfolio_items
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return <PortfolioItem key={item.id} item={item} />;
+    });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <h2>{this.state.pageTitle}</h2>
+
+        <button className="btn" onClick={() => this.handleFilter("Coding")}>
+          Coding
+        </button>
+        <button className="btn" onClick={() => this.handleFilter("Scheduling")}>
+        Scheduling
+        </button>
+        <button className="btn" onClick={() => this.handleFilter("School")}>
+          School
+          </button>
+
+        <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
+      </div>
+    );
+  }
+}
